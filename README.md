8c91884ed541afbe20829e752fb2e6a3ce7fb7ac
adams <ghostry@ghostry.cn>
2021-07-16 10:08:52 +0800

    skype fix

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # docker-nativefier
 docker-nativefier
 # 使用方法
-参考 https://github.com/ghostry/docker-nativefier/tree/master/app
+参考 https://github.com/easy-docker/nativefier/tree/master/app
--- app/skype.sh
+++ app/skype.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 name='skype'
 url='https://web.skype.com/'
-./base.sh "$name" "$url"
+docker run --rm\
+    -v $PWD:/src \
+    -v $PWD:/root/.cache/electron \
+    ghostry/nativefier \
+    nativefier \
+    -m --name "$name" \
+    -p linux -a x64 \
+    --icon /src/icon/$name.png \
+    --single-instance \
+    --tray \
+    --verbose \
+    --min-width 400 \
+    --min-height 400 \
+    --disk-cache-size 52428800 \
+    --file-download-options '{"saveAs":true}' \
+    --internal-urls "(.*?\.live.com)|(.*?\.skype.com)" \
+    "$url" \
+    /src/
+sudo rm -rf /opt/$name-linux-x64
+sudo rm ~/.local/share/applications/$name.desktop
+sudo mv $name-linux-x64 /opt/
+sudo cp -a desktop/$name.desktop /opt/$name-linux-x64/$name.desktop
+ln -s /opt/$name-linux-x64/$name.desktop ~/.local/share/applications/$name.desktop
+echo 'done'
